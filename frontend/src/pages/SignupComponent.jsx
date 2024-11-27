016a270c742f50fd1f33d4e612e527d3af3aeecb
teemvat <teemu.vataja@metropolia.fi>
2024-11-27 10:45:35 +0200

    iteration 4

--- frontend/src/pages/SignupComponent.jsx
+++ frontend/src/pages/SignupComponent.jsx
@@ -5,14 +5,20 @@ import useSignup from "../hooks/useSignup";
 const SignupComponent = ({ setIsAuthenticated }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
   const navigate = useNavigate();
 
 
   const { error, loading, success, handleSignup } = useSignup();
   const signup = async (e) => {
-    e.preventDefault();
-    await handleSignup({ email, password }, { setIsAuthenticated });
-    navigate("/");
+    // iteration4: no changes to backend
+    if (password === password2) {
+      e.preventDefault();
+      await handleSignup({ email, password }, { setIsAuthenticated });
+      navigate("/");
+    } else {
+      alert("Passwords don't match");
+    }
   };
 
   return (
@@ -33,6 +39,14 @@ const SignupComponent = ({ setIsAuthenticated }) => {
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        Repeat password:
+        <input
+          type="password"
+          value={password2}
+          onChange={(e) => setPassword2(e.target.value)}
         />
       </label>
       <br />
